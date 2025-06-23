dec18471a4ff666359c1c3b1bb5e45ad7cd8b446
Marcos Vinicius <marcosbts71@gmail.com>
2025-06-23 16:42:01 -0300

    Remoção projetos denecessários

--- frontend/next-app/src/components/LoadApps/props.ts
+++
@@ -1,3 +0,0 @@
-export interface LoadAppsProps {
-  type: "global" | "checkout";
-}
--- frontend/next-app/src/components/nav/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-
-export { default as NavSectionMini } from './NavSectionMini';
-export { default as NavSectionVertical } from './NavSectionVertical';
-export { default as NavSectionHorizontal } from './NavSectionHorizontal';
