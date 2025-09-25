a82e76426b84c22bea8abd4570bfcf2ed2b9be87
Marcos Vinicius <marcosbts71@gmail.com>
2025-09-25 18:03:50 -0300

    Nova estrutura plugins

--- frontend/src/@app-checkout/Plugin.tsx
+++ frontend/src/@app-checkout/Plugin.tsx
@@ -1,6 +1,7 @@
 import { Props } from './props';
 
-const Plugin = ({ pathname, authData }: Props) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Plugin = (_: Props) => {
   return <>Checkout</>;
 };
 
--- frontend/src/@app-checkout/props.ts
+++ frontend/src/@app-checkout/props.ts
@@ -1,7 +1,3 @@
-import { UserData } from 'types/auth';
 import { PluginProps } from 'types/plugin';
 
-export type Props = PluginProps<{
-  pathname: string;
-  authData?: UserData; // Um plugin global pode não ter authData caso o usuário não esteja logado
-}>;
+export type Props = PluginProps;
