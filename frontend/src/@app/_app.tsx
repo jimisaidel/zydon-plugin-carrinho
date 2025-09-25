a21cd67efc1307f8fc4d618ed5a74f8551b6b7cc
Marcos Vinicius <marcosbts71@gmail.com>
2025-09-25 10:14:28 -0300

    Melhoria geração web componente

--- frontend/src/@app/_app.tsx
+++ frontend/src/@app/_app.tsx
@@ -1,53 +1,7 @@
-import ReactDOM from 'react-dom/client';
+import { defineReactElement } from 'utils/plugin';
 
 import packageJson from '../../package.json';
 
 import AppReact from './App';
 
-class App extends HTMLElement {
-  private mountPoint: HTMLDivElement;
-  private root: ReactDOM.Root | null = null;
-  private _props: Record<string, unknown> = {};
-
-  constructor() {
-    super();
-    this.mountPoint = document.createElement('div');
-  }
-
-  set props(v: Record<string, unknown>) {
-    this._props = v || {};
-    this.render();
-  }
-  get props() {
-    return this._props;
-  }
-
-  connectedCallback() {
-    this.appendChild(this.mountPoint);
-
-    this.upgradeProperty('props');
-
-    if (!this.root) {
-      this.root = ReactDOM.createRoot(this.mountPoint);
-    }
-    this.render();
-  }
-
-  disconnectedCallback() {
-    this.root?.unmount();
-  }
-
-  private upgradeProperty(prop: string) {
-    if (Object.prototype.hasOwnProperty.call(this, prop)) {
-      const value = (this as Record<string, unknown>)[prop];
-      delete (this as Record<string, unknown>)[prop];
-      (this as Record<string, unknown>)[prop] = value;
-    }
-  }
-
-  render() {
-    this.root?.render(<AppReact {...this.props} />);
-  }
-}
-
-customElements.define(packageJson.name, App);
+defineReactElement(AppReact, packageJson.name);
